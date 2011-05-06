b41a1532ac3d0cdd2584fb0af0e08d0478e8d686
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-05-06 07:55:10 -0600

    sketched static pages and a little test server to emulate deployment environment for local dev

---
+++ test/test_server.js
@@ -0,0 +1,53 @@
+/**
+ * ircloggr - a node.js all-in-one implementation of irc logging visible via
+ *            a REST/JSON interface.
+ *
+ * See LICENSE file for licensing information.
+ *
+ * A simple test server for running static web pages and proxying
+ * wsapi to /api.  Allows for local dev mirroing deployment layout.
+ */
+
+const sys = require("sys"),
+     http = require("http"),
+      url = require("url"),
+     path = require("path"),
+       fs = require("fs"),
+  connect = require('connect');
+
+var server = connect.createServer()
+    .use(connect.favicon())
+    .use(connect.logger())
+    .use(function(request, response, next) {
+        var urlpath = url.parse(request.url).pathname;
+        var args = urlpath.substr(1).split('/');
+        if (args[0] == 'api') {
+            // proxy
+            var options = {
+                host: '127.0.0.1',
+                port: 51432,
+                path: '/' + args.slice(1).join('/')
+            };
+            http.get(options, function(res) {
+                response.writeHead(res.statusCode, {
+                    "Content-Type": response.getHeader('content-type')
+                });
+                res.on('data', function(chunk) {                
+                    response.write(chunk);
+                }).on('end', function() {
+                    response.end();
+                });
+            }).on('error', function(e) {
+                response.writeHead(503, {"Content-Type": "text/plain"});
+                response.write("no api server running: " + e + "\n");
+                response.end();
+            });
+        } else {
+            next();
+        }
+    })
+    .use(connect.static(path.join(path.dirname(__dirname), "static")))
+    .listen(60000, "127.0.0.1");
+
+console.log("running on http://127.0.0.1:60000");
+
