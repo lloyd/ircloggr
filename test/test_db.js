fabbeae7662c26ee93714ec93d7a4cde9dc1cd30
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-05-05 18:08:21 -0600

    move test_db outta source and into test/ directory

--- test/test_db.js
+++ test/test_db.js
@@ -1,4 +1,4 @@
-const db = require("./db");
+const db = require("../server/db");
 
 db.log_message("fake.host", "#noroom", "lloyd", "I love ircloggr");
 db.log_message("fake.host", "#noroom", "lloyd", "he is kind and gentle");
